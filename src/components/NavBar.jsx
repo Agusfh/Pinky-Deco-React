3d4feae832ea3158cad1d639e7c5d40f3322367c
agus fernandez <fernandezhagustina@gmail.com>
2023-03-06 12:45:13 -0300

    navbar ok

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,43 +4,41 @@ import './NavBar.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from '../assets/logo.jpg';
 import { border } from '@chakra-ui/react';
+
 
 function NavBar() {
   return (
     <>
     <div className="container-fluid">
-                <div className="row">
-                    <div className="col text-left d-none d-md-block"></div>
-              <div className="col col-md-6 font-small text-center"> 10% OFF x transf. 3 y 6 CUOTAS SIN INTERÉS!</div>        
-              <div className="col text-right d-none d-md-block">
-                      
-                      </div>
-                </div>
-            </div>
-            
-            <Navbar collapseOnSelect expand="lg" className='barra'>
+    <div className="row">
+        <div className="col text-left d-none d-md-block"></div>
+  <div className="col col-md-6 font-small text-center"> 10% OFF x transf. 3 y 6 CUOTAS SIN INTERÉS!</div>        
+  <div className="col text-right d-none d-md-block">
+          
+          </div>
+    </div>
+</div>
+
+<Navbar collapseOnSelect expand="lg" className='barra'>
       <Container>
-        <div className='logotipo'>
-        <Navbar.Brand>
+      <Navbar.Brand>
       <img
               alt=""
               src= {logo}
               width="30"
               height="30"
               className="d-inline-block align-top"
-            />{' '}</Navbar.Brand>
-         <Navbar.Brand>Pinky Deco</Navbar.Brand>
-
-        </div>
-      
+            />{' '}
+          Pinky Deco</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Home</Nav.Link>
-            <Nav.Link href="#pricing">Features</Nav.Link>
-            <Nav.Link href="#action/3.1">Pricing</Nav.Link>
+            <Nav.Link href="#bazar">Bazar</Nav.Link>
+            <Nav.Link href="#deco">Deco</Nav.Link>
+            <Nav.Link href="#mates">Mates</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -52,4 +50,4 @@ function NavBar() {
   );
 }
 
-export default NavBar;
+export default NavBar;
